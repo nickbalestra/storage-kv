cd21954132c6757246e419d7ee1ec7bbca7db4c9
Nick Balestra <nb@cloudflare.com>
2019-05-25 12:19:32 +0100

    setup tests

---
+++ tests/getOptions.test.js
@@ -0,0 +1,65 @@
+/* eslint-disable */
+
+const { join } = require("path");
+const getOptions = require("../src/utils/getOptions");
+const constants = require("../src/constants");
+
+beforeEach(() => {
+  delete process.env.KEYFILENAME;
+  delete process.env.CF_ID;
+  delete process.env.CF_EMAIL;
+  delete process.env.CF_KEY;
+});
+
+test("Passing credential object to getOptions", () => {
+  const credentials = require(join(__dirname, "mockCredentials.json"));
+
+  const options = getOptions(credentials);
+  expect(options.baseURL).toBe(
+    `${constants.CF_API_URL}/accounts/${credentials.id}`
+  );
+  expect(options.headers.common["X-Auth-Email"]).toBe(credentials.email);
+  expect(options.headers.common["X-Auth-Key"]).toBe(credentials.key);
+});
+
+test("Passing path to credentialsFile.json", () => {
+  const credentialsFile = join(__dirname, "mockCredentials.json");
+  const credentials = require(credentialsFile);
+
+  const options = getOptions(credentialsFile);
+
+  expect(options.baseURL).toBe(
+    `${constants.CF_API_URL}/accounts/${credentials.id}`
+  );
+  expect(options.headers.common["X-Auth-Email"]).toBe(credentials.email);
+  expect(options.headers.common["X-Auth-Key"]).toBe(credentials.key);
+});
+
+test("Relying on path available in global env", () => {
+  const credentialsFile = join(__dirname, "mockCredentials.json");
+  const credentials = require(credentialsFile);
+
+  process.env.KEYFILENAME = credentialsFile;
+  const options = getOptions();
+
+  expect(options.baseURL).toBe(
+    `${constants.CF_API_URL}/accounts/${credentials.id}`
+  );
+  expect(options.headers.common["X-Auth-Email"]).toBe(credentials.email);
+  expect(options.headers.common["X-Auth-Key"]).toBe(credentials.key);
+});
+
+test("Relying on credential available in global env", () => {
+  const CF_ID = "002";
+  const CF_KEY = "123ABC";
+  const CF_EMAIL = "john@doe.com";
+
+  process.env.CF_ID = CF_ID;
+  process.env.CF_EMAIL = CF_EMAIL;
+  process.env.CF_KEY = CF_KEY;
+  const options = getOptions();
+
+  expect(options.baseURL).toBe(`${constants.CF_API_URL}/accounts/${CF_ID}`);
+  expect(options.headers.common["X-Auth-Email"]).toBe(CF_EMAIL);
+  expect(options.headers.common["X-Auth-Key"]).toBe(CF_KEY);
+});
