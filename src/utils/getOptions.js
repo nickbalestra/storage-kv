918a7333e7a0ab1dd6b336e76cb6aefb5d41a16d
Nick Balestra <nb@cloudflare.com>
2019-05-26 08:35:36 +0100

    More constants

--- src/utils/getOptions.js
+++ src/utils/getOptions.js
@@ -1,21 +1,30 @@
-/* eslint-disable global-require, no-param-reassign, import/no-dynamic-require, no-nested-ternary */
+/* eslint-disable global-require, no-param-reassign, import/no-dynamic-require, no-nested-ternary, operator-linebreak */
 const { join } = require("path");
-const { CF_API_URL, CREDENTIALS_FILENAME } = require("./../constants.js");
+const {
+  CF_API_URL,
+  CF_CREDENTIALS_FILENAME,
+  CF_KEYFILENAME_ENV_NAME,
+  CF_EMAIL_ENV_NAME,
+  CF_ID_ENV_NAME,
+  CF_KEY_ENV_NAME
+} = require("./../constants.js");
 
 module.exports = credentials => {
   if (typeof credentials === "string") {
     credentials = require(credentials);
   }
   if (!credentials) {
-    credentials = process.env.KEYFILENAME
-      ? require(process.env.KEYFILENAME)
-      : process.env.CF_EMAIL && process.env.CF_ID && process.env.CF_KEY
+    credentials = process.env[CF_KEYFILENAME_ENV_NAME]
+      ? require(process.env[CF_KEYFILENAME_ENV_NAME])
+      : process.env[CF_EMAIL_ENV_NAME] &&
+        process.env[CF_ID_ENV_NAME] &&
+        process.env[CF_KEY_ENV_NAME]
       ? {
-          id: process.env.CF_ID,
-          email: process.env.CF_EMAIL,
-          key: process.env.CF_KEY
+          email: process.env[CF_EMAIL_ENV_NAME],
+          id: process.env[CF_ID_ENV_NAME],
+          key: process.env[CF_KEY_ENV_NAME]
         }
-      : require(join(process.cwd(), CREDENTIALS_FILENAME));
+      : require(join(process.cwd(), CF_CREDENTIALS_FILENAME));
   }
 
   return {
